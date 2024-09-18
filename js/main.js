80ec2dc0b57c1cc4f539c6d0d0bad3174afa88f0
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-09-19 00:12:11 +0500

    finesh

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// HEADER BURGER MENU /////////////////////////
+
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", function () {
+    document.body.classList.toggle("menu-opened")
+});
